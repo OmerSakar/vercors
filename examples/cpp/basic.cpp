25a949710076d40a00e843b511b7bfab30cd098c
Ömer Şakar <o.f.o.sakar@utwente.nl>
2020-07-16 16:35:53 +0200

    Example used for testing.

---
+++ examples/cpp/basic.cpp
@@ -0,0 +1,24 @@
+// Example program
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+  int total = 0;
+  while (total < 5) {
+    total = total + total;
+  }
+  return total;
+
+  if (total == 5) {
+    int i = 0;
+  } else {
+    int plz = 8;
+  }
+  if (total==6) total = 2;
+}
+
+//  for (int i = 0; i < 5; i++) {
+//    total = total + i;
+//  }
